d2a04911498c34583366ca8e02ba19ca77fa9970
koehlek99 <koehlek99@zedat.fu-berlin.de>
2022-04-11 15:36:22 +0200

    changed final data structure

--- workflow/scripts/rf/predict_expression.py
+++ workflow/scripts/rf/predict_expression.py
@@ -5,6 +5,7 @@ import numpy as np
 rf = joblib.load(snakemake.input['MODEL'])
 features = pd.read_csv(snakemake.input['FEATURES'], sep = "\t", index_col = 0, header = 0).fillna(0)
 
-features.loc[:,'predicted'] = rf.predict(np.array(features.drop(["monocytes"], axis = 1)))
+predictions = pd.DataFrame(index = features.index)
+predictions['predictedExpression'] = rf.predict(np.array(features.drop(["monocytes"], axis = 1)))
 
-features.to_csv(snakemake.output['PREDICTIONS'])
+predictions.to_csv(snakemake.output['PREDICTIONS'])
